d533bf7d09b4af802be2a7d3d4efecae327b3d3a
esarr13 <emmanuelbenoitsarr@gmail.com>
2022-07-22 12:45:07 -0400

    chat app - login & logout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { ChatEngine } from "react-chat-engine";
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LogInForm from "./components/LogInForm";
 
 const App = () => {
+    if (!localStorage.getItem('username')) return <LogInForm />
+    
+    const onClick = () => {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+        window.location.reload();
+    }
+
     return (
-        <ChatEngine
-            height="100vh"
-            projectID="8eb88e1f-6c2a-405f-acd1-761fb4d347c5"
-            userName="esarr33"
-            userSecret="Test1234"
-            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>} />
+        <div>
+            <div>
+                <button className="button" onClick={onClick}>
+                    <span>Log Out</span>
+                </button>
+            </div>
+            <ChatEngine
+                height="100vh"
+                projectID="8eb88e1f-6c2a-405f-acd1-761fb4d347c5"
+                userName={localStorage.getItem('username')}
+                userSecret={localStorage.getItem('password')}
+                renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} />
+            )
+        </div>
     )
 }
 
